a49223acdfef061f1dab0d084769a639f0ab8c4f
Justin Dang <justin@justind.me>
2019-06-24 00:21:46 +1000

    create app.py and update run script

--- run.sh
+++ run.sh
@@ -10,9 +10,28 @@ then
 elif [ $1 = "test" ]
 then
     echo "test"
-elif [ $1 = "run" ]
+elif [ $1 = "run" ] 
 then
-    echo "run"
+    OPTIND=2
+    while getopts ":d" opt; do
+        case $opt in
+            d)
+                export FLASK_ENV=developement;
+                export FLASK_DEBUG=1;
+                echo ">>> DEVELOPMENT MODE ACTIVATED";
+                ;;
+            \?)
+                echo "Invalid option: -$OPTARG" >&2;
+                ;;
+            :)
+        esac
+    done
+    if [ $OPTIND -eq 2 ]
+    then
+        echo "Hint: Use 'run -d' for debug mode";
+    fi
+    export FLASK_APP=app.py;
+    pipenv run flask run;
 else
     echo "Invalid command"
 fi
